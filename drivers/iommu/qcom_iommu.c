60bc39741e8c9b3e50b7ac647c9a6b1953c1651b
AngeloGioacchino Del Regno <kholk11@gmail.com>
2019-09-26 11:21:17 +0200

    iommu/qcom: Add support for QCIOMMUv2 and QCIOMMU-500 secured contexts
    This IOMMU is yet another Qualcomm variant of known IOMMUs, found in
    Family-B SoCs, such as MSM8956, MSM8976, MSM8953, MSM8917 and others,
    and that firmware perfectly adheres to this driver logic.
    This time, though, the catch is that the secure contexts are also
    secured, meaning that these are programmed by the bootloader or TZ
    and their "interesting" registers are locked out, so the hypervisor
    disallows touching them from the non-secure world: in this case
    the OS is supposed to blindly trust the secure configuration of
    these contexts and just use them "as they are".
    
    For this reason, it is necessary to distinguish between the v1 and
    500/v2 secure contexts in this driver in order to adhere to this
    specification. To do this, add a new DT compatible, named
    "qcom,msm-iommu-v2-sec" that will trigger the new behavior.
    
    For the sake of completeness, also add a "qcom,msm-iommu-v2-ns" so
    that the human eye gets pleased with it when reading the contexts
    in the final SoC DT. Of course, the latter is just cosmetic.
    
    Signed-off-by: AngeloGioacchino Del Regno <kholk11@gmail.com>

--- drivers/iommu/qcom_iommu.c
+++ drivers/iommu/qcom_iommu.c
@@ -56,6 +56,7 @@ struct qcom_iommu_ctx {
 	struct device		*dev;
 	void __iomem		*base;
 	bool			 secure_init;
+	bool			 secured_ctx;
 	u8			 asid;      /* asid and ctx bank # are 1:1 */
 	struct iommu_domain	*domain;
 };
@@ -279,6 +280,12 @@ static int qcom_iommu_init_domain(struct iommu_domain *domain,
 				goto out_clear_iommu;
 			}
 			ctx->secure_init = true;
+		}
+
+		/* Secured QSMMU-500/QSMMU-v2 contexts cannot be programmed */
+		if (ctx->secured_ctx) {
+			ctx->domain = domain;
+			break;
 		}
 
 		qcom_iommu_reset_ctx(ctx);
@@ -762,10 +769,15 @@ static int qcom_iommu_ctx_probe(struct platform_device *pdev)
 		return -ENODEV;
 	}
 
+	if (of_device_is_compatible(dev->of_node, "qcom,msm-iommu-v2-sec"))
+		ctx->secured_ctx = true;
+
 	/* clear IRQs before registering fault handler, just in case the
 	 * boot-loader left us a surprise:
 	 */
-	iommu_writel(ctx, ARM_SMMU_CB_FSR, iommu_readl(ctx, ARM_SMMU_CB_FSR));
+	if (!ctx->secured_ctx)
+		iommu_writel(ctx, ARM_SMMU_CB_FSR,
+			     iommu_readl(ctx, ARM_SMMU_CB_FSR));
 
 	ret = devm_request_irq(dev, irq,
 			       qcom_iommu_fault,
@@ -807,6 +819,8 @@ static int qcom_iommu_ctx_remove(struct platform_device *pdev)
 static const struct of_device_id ctx_of_match[] = {
 	{ .compatible = "qcom,msm-iommu-v1-ns" },
 	{ .compatible = "qcom,msm-iommu-v1-sec" },
+	{ .compatible = "qcom,msm-iommu-v2-ns" },
+	{ .compatible = "qcom,msm-iommu-v2-sec" },
 	{ /* sentinel */ }
 };
 
@@ -824,7 +838,8 @@ static bool qcom_iommu_has_secure_context(struct qcom_iommu_dev *qcom_iommu)
 	struct device_node *child;
 
 	for_each_child_of_node(qcom_iommu->dev->of_node, child)
-		if (of_device_is_compatible(child, "qcom,msm-iommu-v1-sec"))
+		if (of_device_is_compatible(child, "qcom,msm-iommu-v1-sec") ||
+		    of_device_is_compatible(child, "qcom,msm-iommu-v2-sec"))
 			return true;
 
 	return false;
